f7be893dd9fe1363d303e32524a90d5987b513ff
hwan20c <hwan20c@gmail.com>
2023-01-16 17:57:55 +0900

    dart tour

--- bin/dart_tour.dart
+++ bin/dart_tour.dart
@@ -1,3 +1,25 @@
-void main(List<String> arguments) {
-  print('Hello world');
+class Player {
+  String name;
+  int xp;
+  String team;
+
+  Player({
+    required this.name,
+    required this.xp,
+    required this.team,
+  });
+
+  void sayHello() {
+    print("Hi my name is $name");
+  }
+}
+
+void main() {
+  var nico = Player(name: 'nico', xp: 1200, team: 'red');
+  var potato = nico
+  ..name = 'las'
+  ..xp = 1200000
+  ..team = 'blue'
+  ..sayHello();
+  potato;
 }
